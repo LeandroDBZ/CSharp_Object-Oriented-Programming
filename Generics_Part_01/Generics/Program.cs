1c9a3aecdba1241ad015cefa6476636338b05648
Leandro Araujo <leandro.eries@gmail.com>
2021-10-23 18:19:08 -0300

    Implementing an Integer Array Print Service

--- Generics_Part_01/Generics/Program.cs
+++ Generics_Part_01/Generics/Program.cs
@@ -6,7 +6,19 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=0; i<n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: "+ printService.First());
         }
     }
 }
