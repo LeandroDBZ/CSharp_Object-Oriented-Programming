d9997f136fff937b883cc9f7ed41b2262636f3a0
Leandro Araujo <leandro.eries@gmail.com>
2021-10-24 16:55:21 -0300

    Create first MVC - Department

--- SalesWebApplication/SalesWebApplication/Controllers/HomeController.cs
+++ SalesWebApplication/SalesWebApplication/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SalesWebApplication.Models;
+using SalesWebApplication.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
